955e588225138658719452368112db4f7a73765b
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-09-19 17:00:00 +0300

    creating transactionTypeDef

---
+++ backend/typeDefs/index.js
@@ -0,0 +1,6 @@
+import { mergeTypeDefs } from "@graphql-tools/merge";
+
+import userTypeDef from "./user.typeDef";
+import transactionTypeDef from "./transaction.typeDef";
+
+const mergedTypeDefs = mergeTypeDefs([userTypeDef, transactionTypeDef]);
